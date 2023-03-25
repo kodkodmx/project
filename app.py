f39c22802016f95b8322d3b41bc13c6715971fed
Alberto Haboba <betoh@hotmail.com>
2023-03-25 17:35:38 -0600

    think is ready

--- app.py
+++ app.py
@@ -2,17 +2,40 @@ from flask import Flask, flash, redirect, render_template, request, url_for
 from helpers import apology
 from flask_bootstrap import Bootstrap5
 
-app = Flask(__name__)
+app = Flask(__name__)    
 
 bootstrap = Bootstrap5(app)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"] )
 def index():
-    return render_template('index.html')
+    if request.method == "GET":
+        return render_template('index.html')
+    else:
+        value = request.form.get("info")
+        if value == "do":
+            return render_template('index.html', selection = "do")
+        
+        elif value == "meet":
+            return render_template('index.html', selection = "meet")
+        
+        elif value == "be":
+            return render_template('index.html', selection = "be")
+        
+        elif value == "host":
+            return render_template('index.html', selection = "host")
 
 @app.route('/book')
 def airbnb():
     return render_template('book.html')
 
+@app.route('/rev')
+def rev():
+    return render_template('reviews.html')
+
+@app.route('/insta')
+def insta():
+    return render_template('insta.html')
+
 if __name__ == '__main__':
     app.run()
+
